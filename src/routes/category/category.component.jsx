9fbe739d9efff0a3421e67bc5c0479c63a1df629
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2023-03-21 18:48:45 -0300

    graphql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,64 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import {useState, useEffect, Fragment} from 'react';
+import {useParams} from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import {CategoryContainer, Title} from './category.styles';
+import Spinner from "../../components/spinner/spinner.component";
+import {gql, useQuery} from "@apollo/client";
 
-import { CategoryContainer, Title } from './category.styles';
+const GET_CATEGORY = gql(`
+    query($title: String!) {
+        getCollectionsByTitle(title: $title) {
+            id
+            title
+            items {
+                id
+                name
+                price
+                imageUrl
+            }
+        }
+    }
+`)
 
 const Category = () => {
-  const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+    const {category} = useParams();
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    const { loading, error, data } = useQuery(GET_CATEGORY, {
+        variables: {
+            title: category
+        }
+    })
 
-  return (
-    <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </Fragment>
-  );
+    useEffect(() => {
+        if(data) {
+            const {
+                getCollectionsByTitle: { items }
+            } = data;
+
+            setProducts(items)
+        }
+    })
+
+    console.log(data)
+
+    return (
+        <Fragment>
+            {loading ? <Spinner/> : (
+                <Fragment>
+                    <Title>{category.toUpperCase()}</Title>
+                    <CategoryContainer>
+                        {products &&
+                            products.map((product) => (
+                                <ProductCard key={product.id} product={product}/>
+                            ))}
+                    </CategoryContainer>
+                </Fragment>
+            )}
+        </Fragment>
+    );
 };
 
 export default Category;
